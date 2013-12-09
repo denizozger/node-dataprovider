2c3169583b704cd3a7bd67e641531c7e61de898e
denizozger <denizozger@gmail.com>
2013-12-09 17:40:10 +0000

    new data

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bigMatchData = require('./match.json');
 
 console.log('Process Id: ' + process.pid);
 
@@ -27,8 +28,61 @@ app.use(express.logger());
 
    switch(requestedResourceId)
    {
+    case 'matchesfeed/1/matchcentre':
+      var body = bigMatchData;
+
+      response.setHeader('Content-Length', body.length);
+      response.setHeader('Content-Type', 'text/plain');
+      response.setHeader('Last-Modified', new Date());
+      response.setHeader('Cache-Control', 'max-age=1');
+
+      return response.send(body);
+    case 'matchesfeed/2/matchcentre':
+      var body = bigMatchData;
+
+      response.setHeader('Content-Length', body.length);
+      response.setHeader('Content-Type', 'text/plain');
+      response.setHeader('Last-Modified', new Date());
+      response.setHeader('Cache-Control', 'max-age=1');
+
+      return response.send(body);
+    case 'matchesfeed/3/matchcentre':
+      var body = bigMatchData;
+
+      response.setHeader('Content-Length', body.length);
+      response.setHeader('Content-Type', 'text/plain');
+      response.setHeader('Last-Modified', new Date());
+      response.setHeader('Cache-Control', 'max-age=1');
+
+      return response.send(body);
+    case 'matchesfeed/4/matchcentre':
+      var body = bigMatchData;
+
+      response.setHeader('Content-Length', body.length);
+      response.setHeader('Content-Type', 'text/plain');
+      response.setHeader('Last-Modified', new Date());
+      response.setHeader('Cache-Control', 'max-age=1');
+
+      return response.send(body);
+    case 'matchesfeed/5/matchcentre':
+      var body = bigMatchData;
+
+      response.setHeader('Content-Length', body.length);
+      response.setHeader('Content-Type', 'text/plain');
+      response.setHeader('Last-Modified', new Date());
+      response.setHeader('Cache-Control', 'max-age=1');
+
+      return response.send(body);
+    case 'matchesfeed/6/matchcentre':
+      var body = bigMatchData;
+
+      response.setHeader('Content-Length', body.length);
+      response.setHeader('Content-Type', 'text/plain');
+      response.setHeader('Last-Modified', new Date());
+      response.setHeader('Cache-Control', 'max-age=1');
+
+      return response.send(body);
    case 'matchesfeed/7/matchcentre':
-      resourceToReturn.id = 'matchesfeed/7/matchcentre';
    		resourceToReturn.refereeName = 'Deniz';
    		
       var body = JSON.stringify(resourceToReturn);
@@ -41,7 +95,6 @@ app.use(express.logger());
 
       return response.send(body);
    case 'matchesfeed/8/matchcentre':
-      resourceToReturn.id = 'matchesfeed/8/matchcentre';
    		resourceToReturn.refereeName = 'Engin';  
       resourceToReturn.weather = 'Sunny';
 
@@ -55,7 +108,6 @@ app.use(express.logger());
 
    		return response.send(body);
    case 'matchesfeed/9/matchcentre':
-      resourceToReturn.id = 'matchesfeed/9/matchcentre';
    		resourceToReturn.refereeName = 'Jon'; 
    		
       var body = JSON.stringify(resourceToReturn);
@@ -68,7 +120,6 @@ app.use(express.logger());
 
       return response.send(body);
    case 'matchesfeed/10/matchcentre':
-      resourceToReturn.id = 'matchesfeed/10/matchcentre';
       resourceToReturn.refereeName = 'Jack';  
       
       var body = JSON.stringify(resourceToReturn);
@@ -77,6 +128,28 @@ app.use(express.logger());
         'Content-Type': 'text/plain',
         'Last-Modified': new Date(),
         'Cache-Control': 'max-age=5'
+      }); 
+
+      return response.send(body);
+    case 'matchesfeed/11/matchcentre':
+
+      var body = bigMatchData;
+
+      response.setHeader('Content-Length', body.length);
+      response.setHeader('Content-Type', 'text/plain');
+      response.setHeader('Last-Modified', new Date());
+      response.setHeader('Cache-Control', 'max-age=1');
+
+      return response.send(body);
+
+    case 'matchesfeed/12/matchcentre':
+      var body = '{ some : corrupt = json / data .. ';
+
+      response.writeHead(200, {
+        'Content-Length': body.length,
+        'Content-Type': 'text/plain',
+        'Last-Modified': new Date(),
+        'Cache-Control': 'max-age=1'
       }); 
 
       return response.send(body);
